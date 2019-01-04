3bed42f8fff01be393053c445a4320bc6d6173c4
chongchong.peng <chongchong.peng@yunzhanghu.com>
2019-01-04 09:33:22 +0800

    leetcode 2 addTwoNumbers

--- 002.add-two-numbers_test.go
+++ 002.add-two-numbers_test.go
@@ -1,42 +1,22 @@
 package leetcode
 
 import (
-	"fmt"
 	"testing"
 )
 
 func Test_addTwoNumbers(t *testing.T) {
-	var a = []int{2, 4, 3}
-	var b = []int{5, 6, 4}
-	var l1, l2 *ListNode
-	l1 = &ListNode{a[0], nil}
-	q := l1
-	for i := 1; i < len(a); i++ {
-		q.Next = &ListNode{a[i], nil}
-		q = q.Next
+	l1 := &ListNode{2, &ListNode{4, &ListNode{3, nil}}}
+	l2 := &ListNode{5, &ListNode{6, &ListNode{4, nil}}}
+	r := addTwoNumbers(l1, l2)
+	if r.Val != 7 {
+		t.Fatalf("%+v", r)
 	}
-	k := l1
-	for t != nil {
-		fmt.Print(" -> ", k)
-		k = k.Next
+	r = r.Next
+	if r.Val != 0 {
+		t.Fatalf("%+v", r)
 	}
-	fmt.Println()
-	l2 = &ListNode{b[0], nil}
-	q = l2
-	for i := 1; i < len(b); i++ {
-		q.Next = &ListNode{b[i], nil}
-		q = q.Next
+	r = r.Next
+	if r.Val != 8 {
+		t.Fatalf("%+v", r)
 	}
-	k = l2
-	for t != nil {
-		fmt.Print(" -> ", k)
-		k = k.Next
-	}
-	fmt.Println()
-	result := addTwoNumbers(l1, l2)
-	for result != nil {
-		fmt.Print(" -> ", result)
-		result = result.Next
-	}
-	fmt.Println(l2)
 }
