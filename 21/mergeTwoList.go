80c958152194dd377dded3a95c297aeabaf03ad6
chongchong.peng <chongchong.peng@yunzhanghu.com>
2019-01-16 23:13:52 +0800

    leetcode 25 reverse k group

--- 21/mergeTwoList.go
+++ 21/mergeTwoList.go
@@ -1,8 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"strings"
+)
 
 func main() {
-	fmt.Println("vim-go")
-
+	input := "foo|bar"
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	split := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+		if i := bytes.IndexByte(data, '|'); i >= 0 {
+			return i + 1, data[0:i], nil
+		}
+		if atEOF {
+			return len(data), data[:len(data)], nil
+		}
+		return 0, nil, nil
+	}
+	scanner.Split(split)
+	for scanner.Scan() {
+		if scanner.Text() != "" {
+			fmt.Println(scanner.Text())
+		}
+	}
 }
